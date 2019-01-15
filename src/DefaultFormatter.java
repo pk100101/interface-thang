2e287a27ce89ebf044ed99cf1e619d78f4ab64fc
pk100101 <42978490+pk100101@users.noreply.github.com>
2019-01-15 09:30:34 -0500

    Update DefaultFormatter.java

--- src/DefaultFormatter.java
+++ src/DefaultFormatter.java
@@ -1,4 +1,4 @@
-public class DefaultFormatter 
+public class DefaultFormatter implements NumberFormatter
 {
 	public String format (int numberInput)
 	{
@@ -7,11 +7,16 @@ public class DefaultFormatter
 		for (int i = number.length()-1; i>0; i--)
 		{
 			int place = 0;
-			if (place % 3 == 0)
+			if (place % 3 == 0 && i>0)
 				newString += ",";
 			else
-				newString += ;
+				newString += numberInput.get(i);
 		}
-		return newString;
+		String finalString = "";
+		for (int i = newString.length()-1; i>0; i--)
+		{
+			finalString += newString.charAt(i);
+		}
+		return finalString;
 	}
 }
